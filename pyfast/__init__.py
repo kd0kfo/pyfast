e85cf4ebea992854ae9f4ebb960b9e627296fc27
David Coss <David.Coss@stjude.org>
2013-09-12 09:34:45 -0500

    Initial commit

---
+++ pyfast/__init__.py
@@ -0,0 +1,19 @@
+# Init
+
+
+class Cukier():
+    def __init__(self, omega_0_list, delta_omega_list):
+        self.omega_0_list = omega_0_list
+        self.delta_omega_list = delta_omega_list
+
+    def freq(self, m, i=1, omega_0=0):
+        if i <= 1:
+            omega = self.omega_0_list[m - 1]
+            return [omega] + list(self.freq(m, i + 1, omega))
+
+        o = omega_0 + self.delta_omega_list[m - i]
+
+        if i == m:
+            return [o]
+
+        return [o] + self.freq(m, i + 1, o)
